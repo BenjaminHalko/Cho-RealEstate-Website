e7fd48446b5ee50427c90930c833c0b199aabb8f
BenjaminHalko <benjaminhalko@hotmail.com>
2023-04-30 16:50:26 -0700

    Added Card Effect

---
+++ public/js/presale_card.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    const cards = document.querySelectorAll('.presale_card');
+
+    function map(val, minA, maxA, minB, maxB) {
+        return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+    }
+
+    function PresaleCard(card, ev) {
+        let inner = card.querySelector('.card_inner');
+        let mouseX = ev.offsetX;
+        let mouseY = ev.offsetY;
+        let rotateY = map(mouseX, 0, 300, -20, 20);
+        let rotateX = map(mouseY, 0, 400, 20, -20);
+        let brightness = map(mouseY, 0, 400, 1.5, 0.5);
+        console.log(mouseX, mouseY, rotateX, rotateY, brightness)
+
+        inner.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        inner.style.filter = `brightness(${brightness})`;
+    }
+
+    // Set up the cards
+    cards.forEach((card) => {
+        card.addEventListener('mousemove', (ev) => {
+            PresaleCard(card, ev);
+        });
+        card.addEventListener('mouseleave', (ev) => {
+            let inner = card.querySelector('.card_inner');
+            inner.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            inner.style.filter = 'brightness(1)';
+        });
+    });
+};
