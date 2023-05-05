e2ba1e75ed835c00aa60a6e3a427342b2010f5b0
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2023-05-05 11:43:25 -0700

    Updated

--- public/js/presale.js
+++ public/js/presale.js
@@ -2,14 +2,15 @@ addEventListener("load", (event) => {
     const mainBackground = document.getElementById('mainBackground');
 
     const cards = document.querySelectorAll('.presale_card');
-    let currentBackground = "contour/contour.png";
+    let currentBackground = 0;
 
     cards.forEach((card) => {
         card.addEventListener('mouseenter', (ev) => {
             const fade = document.createElement('span');
             mainBackground.appendChild(fade);
             fade.classList = 'main-background fading';
-            fade.style.setProperty("background-image",`url('/images/presale/${Math.random() > 0.5 ? 'contour/contour.png' : 'test/test.jpeg'}'`);
+            currentBackground = 1-currentBackground;
+            fade.style.setProperty("background-image",`url('/images/presale/${currentBackground ? 'contour/contour.png' : 'test/test.jpeg'}'`);
             fade.addEventListener('animationend', (ev) => {
                 mainBackground.style.setProperty('background-image',fade.style.getPropertyValue('background-image'));
                 fade.remove();
