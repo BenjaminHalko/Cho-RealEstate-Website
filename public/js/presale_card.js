a333d0c23248cad131ef8a9a0530e190ab8707f4
Benjamin <73490201+BenjaminHalko@users.noreply.github.com>
2023-05-12 11:32:45 -0700

    Updated Card in Mobile View

--- public/js/presale_card.js
+++ public/js/presale_card.js
@@ -7,16 +7,21 @@ addEventListener("load", (event) => {
 
     function PresaleCard(card, ev) {
         let inner = card.querySelector('.card_inner');
-        let cardWidth = card.offsetWidth;
-        let cardHeight = card.offsetHeight;
-        let mouseX = ev.offsetX;
-        let mouseY = ev.offsetY;
-        let rotateY = map(mouseX, 0, cardWidth, -20, 20);
-        let rotateX = map(mouseY, 0, cardHeight, 20, -20);
-        let brightness = map(mouseY, 0, cardHeight, 1.5, 0.5);
+        if (window.innerWidth >= 700) {
+            let cardWidth = card.offsetWidth;
+            let cardHeight = card.offsetHeight;
+            let mouseX = ev.offsetX;
+            let mouseY = ev.offsetY;
+            let rotateY = map(mouseX, 0, cardWidth, -20, 20);
+            let rotateX = map(mouseY, 0, cardHeight, 20, -20);
+            let brightness = map(mouseY, 0, cardHeight, 1.5, 0.5);
 
-        inner.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        inner.style.filter = `brightness(${brightness})`;
+            inner.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+            inner.style.filter = `brightness(${brightness})`;
+        } else {
+            inner.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            inner.style.filter = 'brightness(1)';
+        }
     }
 
     // Set up the cards
