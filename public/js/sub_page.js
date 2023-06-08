3c37c73c1bd14486f76ef972dc1069403c36bb7b
BenjaminHalko <benjaminhalko@hotmail.com>
2023-06-08 10:19:31 -0700

    Updated

--- public/js/sub_page.js
+++ public/js/sub_page.js
@@ -2,8 +2,7 @@ addEventListener("load", (event) => {
     const panels = document.querySelectorAll('.panel, .floating-btn, .floating-img');
 
     for (let i = 0; i < panels.length; i++) {
-        panels[i].style.animationName = 'fade_in';
-        panels[i].style.animationDelay = `${i * 0.2}s`;
+        panels[i].style.setProperty('--delay',`${i * 0.2}s`);
     }
     
     document.querySelector('.intro').classList.remove('intro');
