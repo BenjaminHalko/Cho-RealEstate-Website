3440130aae6d5b58b9121d6d24b415b5afda8733
BenjaminHalko <benjaminhalko@hotmail.com>
2023-08-08 10:21:15 -0700

    Updated Newsletter Signup Form

--- public/js/newsletter.js
+++ public/js/newsletter.js
@@ -2,19 +2,23 @@ addEventListener("load", (event) => {
     const form = document.querySelector('form')
     form.onsubmit = function(event) {
         event.preventDefault();
-        const email = form.querySelector('input').value;
+        const name = form.querySelector('#name').value;
+        const email = form.querySelector('#email').value;
         
         // Disable button & input
-        form.querySelector('input').disabled = true;
+        form.querySelectorAll('input').forEach(element => {
+            element.disabled = true;
+        });
         form.querySelector('button').disabled = true;
 
         // Replace input text
-        form.querySelector('input').value = 'Thanks for subscribing!';
+        form.querySelector('button').innerHTML = 'Thank you!';
 
         // Send request
         const request = new XMLHttpRequest();
         request.open('POST', '/php/newsletter.php', true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send('name=' + name);
         request.send('email=' + email);
         return false;
     }
