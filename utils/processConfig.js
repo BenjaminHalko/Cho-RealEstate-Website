aa2429b4b05729a7febcd6bf733fd0c444ba66e8
BenjaminHalko <benjaminhalko@hotmail.com>
2023-08-29 10:13:40 -0700

    Cleaned up files

--- utils/processConfig.js
+++ utils/processConfig.js
@@ -1,44 +1,44 @@
 // Import modules
 const fs = require('fs');
-const path = require('path');
 
 function processLocations() {
-    console.log("Processing locations");
-    const locationData = require('../config/location.json');
+    console.log("Processing properties");
+    const properties = require('../config/properties.json');
 
     // Loop through locations
-    for (let location in locationData.locations) {
-        locationData.locations[location].id = location;
-        if (!locationData.locations[location].isListing) { locationData.locations[location].isListing = false; }
+    for (let pName in properties) {
+        const property = properties[pName];
+        property.id = pName;
+        if (!property.isListing) { property.isListing = false; }
         
         // Load Images
-        if (fs.existsSync(path.resolve(__dirname, '..', 'public', 'images', location))) {
-            locationData.locations[location].images = fs.readdirSync(path.resolve(__dirname, '..', 'public', 'images', location));
-            const cardPath = locationData.locations[location].images.find(filename => filename.match(/card.*/g));
-            const backgroundPath = locationData.locations[location].images.find(filename => filename.match(/background.*/g));
+        if (fs.existsSync(`./public/images/${property.id}`)) {
+            property.images = fs.readdirSync(`./public/images/${property.id}`);
+            const cardPath = property.images.find(filename => filename.match(/card.*/g));
+            const backgroundPath = property.images.find(filename => filename.match(/background.*/g));
             
             if (cardPath) {
-                locationData.locations[location].card_image = cardPath;
-                locationData.locations[location].images.splice(locationData.locations[location].images.indexOf(cardPath), 1);
-                locationData.locations[location].images.push(cardPath);
+                property.card_image = cardPath;
+                property.images.splice(property.images.indexOf(cardPath), 1);
+                property.images.push(cardPath);
             }
 
             if (backgroundPath) {
-                locationData.locations[location].background = backgroundPath;
-                locationData.locations[location].images.splice(locationData.locations[location].images.indexOf(backgroundPath), 1);
-                locationData.locations[location].images.push(backgroundPath);
+                property.background = backgroundPath;
+                property.images.splice(property.images.indexOf(backgroundPath), 1);
+                property.images.push(backgroundPath);
             }
         }
 
         // Load PDFs
-        locationData.locations[location].pdfs = [];
-        if (fs.existsSync(path.resolve(__dirname, '..', 'public', 'files', location))) {
+        property.pdfs = [];
+        if (fs.existsSync(`./public/files/${property.id}`)) {
             let brochureData = null;
             let floorplanData = [];
-            for(let file of fs.readdirSync(path.resolve(__dirname, '..', 'public', 'files', location))) {
+            for(let file of fs.readdirSync(`./public/files/${property.id}`)) {
                 const pdfData = {
                     'file': file,
-                    'name': file.replace(location+'-', '').replace('.pdf', '').split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+                    'name': file.replace(property.id+'-', '').replace('.pdf', '').split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
                 };
                 if (pdfData.name === 'Brochure') {
                     pdfData.icon = 'card-list';
@@ -48,70 +48,63 @@ function processLocations() {
                     floorplanData.push(pdfData);
                 } else {
                     pdfData.icon = 'info-circle';
-                    locationData.locations[location].pdfs.push(pdfData);
+                    property.pdfs.push(pdfData);
                 }
             }
 
             // Add brochure/floorplan
             if (floorplanData != []) {
-                locationData.locations[location].pdfs = floorplanData.concat(locationData.locations[location].pdfs);
+                property.pdfs = floorplanData.concat(property.pdfs);
             }
             if (brochureData) {
                 if (!floorplanData) {
                     brochureData.name = 'Brochure/Floorplan';
                 }
-                locationData.locations[location].pdfs.unshift(brochureData);
+                property.pdfs.unshift(brochureData);
             }
         }
 
         // State
-        if (typeof locationData.locations[location].state === 'undefined') {
-            locationData.locations[location].state = "default";
+        if (typeof property.state === 'undefined') {
+            property.state = "default";
         }
-    }
-
-    // Set featured location
-    locationData.featured = locationData.locations[locationData.featured];
-    if (typeof locationData.secondary !== 'undefined') {
-        locationData.secondary = locationData.locations[locationData.secondary];
-    } else {
-        locationData.secondary = null;
     }
 
     // Set up sorted locations
-    locationData.sortedLocations = {};
-    for (let location of Object.values(locationData.locations)) {
-        if (location.state == 'featured') {
-            if (typeof locationData.sortedLocations['Featured'] === 'undefined') {
-                locationData.sortedLocations['Featured'] = [location];
+    let sortedProperties = {};
+    for (let property of Object.values(properties)) {
+        if (property.state == 'featured') {
+            if (typeof sortedProperties['Featured'] === 'undefined') {
+                sortedProperties['Featured'] = [property];
             } else {
-                locationData.sortedLocations['Featured'].push(location);
+                sortedProperties['Featured'].push(property);
             }
         }
 
-        if (typeof locationData.sortedLocations[location.catagory] === 'undefined') {
-            locationData.sortedLocations[location.catagory] = {};
+        if (typeof sortedProperties[property.catagory] === 'undefined') {
+            sortedProperties[property.catagory] = {};
         }
 
-        if (typeof locationData.sortedLocations[location.catagory][location.location] === 'undefined') {
-            locationData.sortedLocations[location.catagory][location.location] = [location];
+        if (typeof sortedProperties[property.catagory][property.location] === 'undefined') {
+            sortedProperties[property.catagory][property.location] = [property];
         } else {
-            locationData.sortedLocations[location.catagory][location.location].push(location);
+            sortedProperties[property.catagory][property.location].push(property);
         }
     }
 
     // Sort locations
-    for (let name of Object.keys(locationData.sortedLocations)) {
+    for (let name of Object.keys(sortedProperties)) {
         if (name == 'Featured') {
-            locationData.sortedLocations[name] = locationData.sortedLocations[name].sort((a,b) => { return a.name.localeCompare(b.name) });
+            sortedProperties[name] = sortedProperties[name].sort((a,b) => { return a.name.localeCompare(b.name) });
             continue;
         }
-        for(let location of Object.keys(locationData.sortedLocations[name])) {
-            locationData.sortedLocations[name][location] = locationData.sortedLocations[name][location].sort((a,b) => { return a.name.localeCompare(b.name) + ((a.state == "sold") - (b.state == "sold"))*2 + ((a.state == "rented") - (b.state == "rented"))*3 + ((b.state == "featured") - (a.state == "featured"))*4 });
+        for(let location of Object.keys(sortedProperties[name])) {
+            sortedProperties[name][location] = sortedProperties[name][location].sort((a,b) => { return a.name.localeCompare(b.name) + ((a.state == "sold") - (b.state == "sold"))*2 + ((a.state == "rented") - (b.state == "rented"))*3 + ((b.state == "featured") - (a.state == "featured"))*4 });
         }
-        locationData.sortedLocations[name] = Object.entries(locationData.sortedLocations[name]).sort((a,b) => { return a[0].localeCompare(b[0]) });
+        sortedProperties[name] = Object.entries(sortedProperties[name]).sort((a,b) => { return a[0].localeCompare(b[0]) });
     }
-    locationData.sortedLocations = Object.entries(locationData.sortedLocations).sort((a,b) => {
+
+    sortedProperties = Object.entries(sortedProperties).sort((a,b) => {
         if (a[0] == 'Featured') return -1;
         if (b[0] == 'Featured') return 0;
         const aLength = a[1].reduce((acc, cur) => {
@@ -123,8 +116,18 @@ function processLocations() {
         return bLength - aLength;
     });
 
+    // Get featured location
+    const featureData = require('../config/featured.json');
+    const featured = properties[featureData.featured];
+    const secondary = (typeof featureData.secondary !== 'undefined') ? properties[featureData.secondary] : null;
+
     // Return data
-    return locationData;
+    return {
+        locations: properties,
+        sortedLocations: sortedProperties,
+        featured: featured,
+        secondary: secondary
+    };
 }
 
 function processReviews() {
@@ -137,7 +140,7 @@ function processNewsletters() {
     console.log("Processing newsletters");
     const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
     const newsletters = [];
-    for (file of fs.readdirSync(path.resolve(__dirname, '..', 'public', 'files', 'newsletters')).reverse()) {
+    for (file of fs.readdirSync("./public/files/newsletters").reverse()) {
         newsletters.push({
             file: file,
             name: months[Number(file.split('_')[1].split('.')[0])-1] + ' ' + file.split('_')[0]
