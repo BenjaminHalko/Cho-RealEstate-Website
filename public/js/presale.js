369c0c0f05695149c68d513f094507c61a2af5e2
Benjamin Halko <benjaminhalko@hotmail.com>
2023-09-25 15:09:27 -0700

    removed presale background fade

--- public/js/presale.js
+++ public/js/presale.js
@@ -1,19 +1,4 @@
 (function() {
-    const mainBackground = document.querySelector('.main-background');
-    document.querySelectorAll('.location_card').forEach((card) => {
-        card.addEventListener('mouseenter', (ev) => {
-            const fade = document.createElement('span');
-            mainBackground.appendChild(fade);
-            fade.classList = 'main-background fading';
-            fade.style.setProperty("background-image",`url('/images/${card.style.getPropertyValue('--background')}')`);
-            fade.addEventListener('animationend', (ev) => {
-                mainBackground.style.setProperty('background-image',fade.style.getPropertyValue('background-image'));
-                fade.remove();
-            });
-            
-        });
-    });
-
     const catagories = document.querySelector('.catagories');
     const catagoryCollapse = {};
     document.querySelectorAll('.catagory').forEach((catagory) => {
