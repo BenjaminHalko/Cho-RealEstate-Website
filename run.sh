2a52b8cb14e378485d475d6b8ae74e3826a7b35b
Benjamin Halko <benjaminhalko@hotmail.com>
2024-01-19 08:36:52 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-npm install
-npm run build
-npm run purgecss
+cloudlinux-selector install-modules --json --interpreter nodejs --app-root repository
+cloudlinux-selector run-script --json --interpreter nodejs --app-root repository --script-name build
+cloudlinux-selector run-script --json --interpreter nodejs --app-root repository --script-name purgecss
 rsync --delete -acv --exclude=".*" ~/repository/build/ ~/repository/public/ ~/public_html
 cloudlinux-selector stop --json --interpreter nodejs --app-root repository
 pkill -9 node
